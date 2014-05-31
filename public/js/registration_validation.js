8b4090ff7665263b7cc6abcbd7729a485c68c004
Krzysztof Ochędzan <jurasik@tlen.pl>
2014-05-31 11:05:49 +0200

    validation fix

--- public/js/registration_validation.js
+++ public/js/registration_validation.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
           password = $("#password").val();
           pw_confirm = $("#confirm_password").val();
           if(nick.length === 0 || password.length === 0 || pw_confirm.length === 0){
-            	$('#error_message').html("Pola nie mogą być puste");
+            	$('#error_message').html("Pola nie mogą być puste.");
             	event.preventDefault();
           }else{
-          	validate(event,[nick,password,confirm]);
+          	validate(event,[nick,password,pw_confirm]);
           }
    });
 
@@ -16,16 +16,16 @@ $(document).ready(function() {
 		var Regex = /^[a-z]*/;
 		var ErrorOccured = false;
 		if(!Regex.test(data[0])){
-			$('#nick_error').html("Nie wprowadzaj cyfr.");
+			$('#error_message').html("Nie wprowadzaj cyfr.");
 			ErrorOccured = true;
 		}
 		if(!NickExists(data[0])){
 			ErrorOccured = true;
-			$("#nick_error").html("Nick zajęty.");
+			$('#error_message').html("Nick zajęty.");
 		}
-		if(!confirmPasswords(data[1],data[2])){
+		if(data[2]!==data[1]){
 			ErrorOccured = true;
-			$("#confirm_password_error").html("Nie zgadza się.");
+			$('#error_message').html("Hasła się nie zgadzają.");
 		}
 		if(ErrorOccured){
 			event.preventDefault();
@@ -33,21 +33,9 @@ $(document).ready(function() {
 			alert("Dodano");
 		}
 	 };
-
-	 var confirmPasswords = function (a, b) {
-	 	if(a === b)
-	 	{
-	 		return true;
-	 	}
-	 	else
-	 	{
-	 		return false;
-	 	}
-	 };
-
 	 var NickExists = function(nick) {
 	 	return true;
-	 }
+	 };
 });
 
 
