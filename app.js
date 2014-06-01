c60236a941505d1e758f1b5e77c81295d96add85
Krzysztof Ochędzan <jurasik@tlen.pl>
2014-06-01 18:33:50 +0200

    val update

--- app.js
+++ app.js
@@ -36,27 +36,37 @@ app.use(passport.session());
 app.use(express.static('public'));
 
 
+///////////////////////////////////////////
+//     logowanie - passport
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function (user, done)
+{
     done(null, user);
 });
 
-passport.deserializeUser(function (obj, done) {
+passport.deserializeUser(function (obj, done)
+{
   done(null, obj);
 });
 
-passport.use(new LocalStrategy(function (username, password, done) {
+passport.use(new LocalStrategy(function (username, password, done)
+{
 	client = mysql.createConnection(sqlInfo);
-	client.query("select nick,admin from users where nick='"+username+"' AND password='"+password+"';",function (err,rows){
+	client.query("select nick,admin from users where nick='"+username+"' AND password='"+password+"';",function (err,rows)
+	{
 		var user;
-		if(typeof rows !== 'undefined'){
+		console.log(rows);
+		if(typeof rows !== 'undefined')
+		{
 			user = rows[0];
 		}
-		if(err){
+		if(err)
+		{
 			return done(err);           
 		}
-		if(!user){
-			return done(null,false);           
+		if(!user)
+		{
+			return done(null, false);           
 		}
 		return done(null, {username: user.nick, admin: user.admin});
 	});
@@ -73,73 +83,131 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static('public'));
 
-/////////////////////////////routes//////////////////////////////////////////////
+///////////////////////////////////////////
+//     przekierowania
+app.post('/login', passport.authenticate('local',
+{
+	successRedirect: '/logged',
+	failureRedirect: '/fail',
+	failureFlash: true 
+}));
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/logged',
-                                   failureRedirect: '/fail',
-                                   failureFlash: true })
-);
-
-app.get('/login', function (req, res) {
+app.get('/login', function (req, res)
+{
 	return res.redirect('login.html');
 });
 
-app.get('/fail', function (req, res) {
+app.get('/fail', function (req, res)
+{
 	return res.redirect('login.html');
 });
 
-app.get('/', function(req, res) {
+app.get('/admin.html', function (req, res)
+{
+	return res.redirect('/');
+});
+
+app.get('/', function(req, res)
+{
     if (req.user) {
 		return res.redirect('/logged');
     } 
-    else {
+    else 
+    {
         return res.redirect('/login');
     }
 });
 
-app.get('/logout', function (req, res) {
-    console.log('Zakończenie sesji użytkownika');
-    req.logout();
-    client.end(function(err) {
-    console.log("Rozłączono z bazą danych");
-  });
-    return res.redirect('/');
+app.get('/logout', function (req, res)
+{
+	console.log('Zakończenie sesji użytkownika');
+	req.logout();
+	client.end(function(err)
+	{
+		console.log("Rozłączono z bazą danych");
+	});
+	return res.redirect('/');
 });
 
-app.get('/logged', function (req, res) {
-  if(req.user && req.user.admin === 'admin'){
+app.get('/logged', function (req, res)
+{
+  if(req.user && req.user.admin === 'admin')
+  {
       return res.redirect('admin.html');
   }
-  else{
+  else
+  {
      return res.redirect('logged.html');
   }
 });
 
-
-// rejestracja usera
-app.get('/signup', function (req, res) {
+///////////////////////////////////////////
+//     rejestracja usera
+app.get('/signup', function (req, res)
+{
 	return res.redirect('signup.html');
 });
 
-app.post('/register_user', function (req, res) {
+app.post('/register_user', function (req, res)
+{
 	var data = req.body;
 	delete data['confirm_password'];
 	registerUser(data);
 	return res.redirect('/');
 });
-var registerUser = function(data) {
+
+var registerUser = function(data)
+{
     client = mysql.createConnection(sqlInfo);
-    var sql = client.query('INSERT INTO users SET ? ;',data,function (err,rows){
-	console.log("registered");
-    if(err){
-      console.log(err);           
-    }
-  });
+   	var sql = client.query('INSERT INTO users SET ? ;',data,function (err,rows)
+    {
+		console.log("registered");
+	    if(err)
+	    {
+	    	console.log(err);           
+	    }
+  	});
 };
+
+///////////////////////////////////////////
+//     mysql validation
+
+app.post('/user_existance', function (req, res)
+{
+	var nick = req.body.nick;
+	console.log(nick);
+    client = mysql.createConnection(sqlInfo);
+    client.query("select nick from users where nick='"+nick+"';",function (err,rows)
+    {
+		console.log(rows);
+		if(err)
+		{
+			console.log(err);           
+		}
+		if(rows)
+		{
+			if(rows[0])
+			{
+		    	return res.send(true);  
+		  	}
+		  	else
+		  	{
+		    	return res.send(false);  
+		  	}
+		}
+		else
+		{
+			console.log("not found in db");
+		   return res.send(false);  
+		}
+	});
+});
+///////////////////////////////////////////
+//     serwer start
 
 server = http.createServer(app);
 
-server.listen(3000, function () {
+server.listen(3000, function ()
+{
     console.log('Serwer pod adresem http://localhost:3000/');
 });
--- public/js/registration_validation.js
+++ public/js/registration_validation.js
@@ -1,40 +1,69 @@
-$(document).ready(function() {
-	$('#confirm_registration').click(function(event) {
+$(document).ready(function()
+{
+	$('#confirm_registration').click(function(event) 
+	{
 		$('.error').empty();
-      var nick = $("#nick").val(),
-          password = $("#password").val();
-          pw_confirm = $("#confirm_password").val();
-          if(nick.length === 0 || password.length === 0 || pw_confirm.length === 0){
-            	$('#error_message').html("Pola nie mogą być puste.");
-            	event.preventDefault();
-          }else{
-          	validate(event,[nick,password,pw_confirm]);
-          }
-   });
+		console.log("Validating registration form:");
+		var nick = $("#nick").val(),
+		password = $("#password").val();
+		pw_confirm = $("#confirm_password").val();
+		if(nick.length === 0 || password.length === 0 || pw_confirm.length === 0)
+		{
+			$('#error_message').html("Pola nie mogą być puste.");
+			event.preventDefault();
+		}
+		else
+		{
+			validate(event,[nick,password,pw_confirm]);
+		}
+	});
 
-	 var validate = function (event,data) {
-		var Regex = /^[a-z]*/;
+	 var validate = function (event,data)
+	 {
+		var Regex = /[^0-9]+/g;
 		var ErrorOccured = false;
-		if(!Regex.test(data[0])){
-			$('#error_message').html("Nie wprowadzaj cyfr.");
+		if(!Regex.test(data[0]))
+		{
+			$('#error_message').html("Nick nie może zaczynać się od cyfry.");
 			ErrorOccured = true;
+			console.log("Nick zaczyna sie od cyfry.");
 		}
-		if(!NickExists(data[0])){
-			ErrorOccured = true;
-			$('#error_message').html("Nick zajęty.");
-		}
-		if(data[2]!==data[1]){
+		var nick_to_check = $('#nick').val();
+		console.log("Nick do sprawdzenia: "+nick_to_check);
+		$.ajax(
+			{
+				type: "POST",
+				url: "/user_existance",
+				dataType: "json",
+				contentType: "application/json",
+				data: JSON.stringify({nick: nick_to_check}),
+				async: false, 
+				success: function (response)
+				{             
+					if(response)
+					{
+						$('#error_message').html('Nick zajęty.');
+						ErrorOccured = true;
+					}
+				}
+			});
+        if(data[2]!==data[1])
+        {
 			ErrorOccured = true;
 			$('#error_message').html("Hasła się nie zgadzają.");
+			console.log("Hasła się nie zgadzają.");
 		}
-		if(ErrorOccured){
+		
+		if(ErrorOccured)
+		{
 			event.preventDefault();
-		}else{
+			console.log("Errory, nie mogę dodać.");
+		}
+		else
+		{
+			console.log("Dodaję.");
 			alert("Dodano");
 		}
-	 };
-	 var NickExists = function(nick) {
-	 	return true;
 	 };
 });
 
